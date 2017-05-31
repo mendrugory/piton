ad98f5a4b5ab24c18602a17c5728bc358adde366
gonzalo <gj@palmtreestatistics.com>
2017-05-31 12:59:14 +0200

    python instead of python3 for the tests

--- test/piton_port_test.exs
+++ test/piton_port_test.exs
@@ -3,31 +3,31 @@ defmodule PitonPortTest do
   
 
   test "Generic Port. Python Fib function" do
-    {:ok, python_port} = MyPort.start([path: Path.expand("test/pythons_test"), python: "python3"], [])
+    {:ok, python_port} = MyPort.start([path: Path.expand("test/pythons_test"), python: "python"], [])
     result = MyPort.execute(python_port, :functions, :fib, [10])
     assert(result == 89)
   end
 
   test "Generic Port. Multiple Python Fib function through Piton" do
-    {:ok, python_port} = MyPort.start([path: Path.expand("test/pythons_test"), python: "python3"], [])
+    {:ok, python_port} = MyPort.start([path: Path.expand("test/pythons_test"), python: "python"], [])
     results = for i <- 1..20, do: MyPort.execute(python_port, :functions, :fib, [i])
     assert(results == [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946])
   end
 
   test "Generic Port. Python Fib function string module" do
-    {:ok, python_port} = MyPort.start([path: Path.expand("test/pythons_test"), python: "python3"], [])
+    {:ok, python_port} = MyPort.start([path: Path.expand("test/pythons_test"), python: "python"], [])
     result = MyPort.execute(python_port, "functions", :fib, [10])
     assert(result == 89)
   end
 
   test "Generic Port. Python Fib function string function" do
-    {:ok, python_port} = MyPort.start([path: Path.expand("test/pythons_test"), python: "python3"], [])
+    {:ok, python_port} = MyPort.start([path: Path.expand("test/pythons_test"), python: "python"], [])
     result = MyPort.execute(python_port, :functions, "fib", [10])
     assert(result == 89)
   end
 
   test "Generic Port. Python Fib function string module and function" do
-    {:ok, python_port} = MyPort.start([path: Path.expand("test/pythons_test"), python: "python3"], [])
+    {:ok, python_port} = MyPort.start([path: Path.expand("test/pythons_test"), python: "python"], [])
     result = MyPort.execute(python_port, "functions", "fib", [10])
     assert(result == 89)
   end
--- test/piton_test.exs
+++ test/piton_test.exs
@@ -5,7 +5,7 @@ defmodule PitonTest do
   test "My own Python Port" do
     defmodule MyPythonFibCalculator do
       use Piton.Port
-      def start_link(), do: MyPythonFibCalculator.start_link([path: Path.expand("test/pythons_test"), python: "python3"], [name: __MODULE__])
+      def start_link(), do: MyPythonFibCalculator.start_link([path: Path.expand("test/pythons_test"), python: "python"], [name: __MODULE__])
       def fib(n), do: MyPythonFibCalculator.execute(__MODULE__, :functions, :fib, [n])
     end
     {:ok, _} = MyPythonFibCalculator.start_link()
