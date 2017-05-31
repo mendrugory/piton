7c394875b3baf4a14022dd28762f7af0fa813102
gonzalo <gj@palmtreestatistics.com>
2017-05-31 12:55:45 +0200

    python instead of python3 for the tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -5,7 +5,7 @@ end
 defmodule MyPythonFibCalculator do
   use Piton.Port
   @timeout        5000
-  def start(), do: MyPythonFibCalculator.start([path: Path.expand("test/pythons_test"), python: "python3"], [])
+  def start(), do: MyPythonFibCalculator.start([path: Path.expand("test/pythons_test"), python: "python"], [])
   def fib(python, n, timeout \\ @timeout), do: MyPythonFibCalculator.execute(python, :functions, :fib, [n], timeout)
   def fib_delay(python, n, delay, timeout \\ @timeout) do
     Process.sleep(delay)
